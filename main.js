d18d26524e55ba24cd2d439e6b03eab24a1b74d5
OdysseyDev <93072230+StickOdyssey@users.noreply.github.com>
2023-11-16 20:50:35 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 /* Set the width of the sidebar to 250px (show it) */
-var sidebar = document.getElementById("mySidepanel");
 function openNav() {
-  sidebar.style.width = "250px";
+  document.getElementById("mySidepanel").style.width = "250px";
 }
 
 /* Set the width of the sidebar to 0 (hide it) */
 function closeNav() {
-  sidebar.style.width = "0";
+  document.getElementById("mySidepanel").style.width = "0";
 }
